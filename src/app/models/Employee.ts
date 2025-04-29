36445ddab88853020b70d1fe85d1f6b8aacf4182
malshanperera123 <kavindumalshan1110@gmail.com>
2025-04-29 17:28:38 +0530

    employee service

--- src/app/models/Employee.ts
+++
@@ -1,8 +0,0 @@
-export interface Employee {
-    id?: number;
-    name: string;
-    email: string;
-    department: 'HR' | 'IT' | 'Finance' | 'Operations';
-    createdAt?: string;
-    updatedAt?: string;
-  }
